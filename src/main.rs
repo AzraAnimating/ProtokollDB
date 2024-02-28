cc8d56b1ccb23312bf3650399f8aa9bfe1532bb0
Tobias Rempe <tobiasrempe@zyonicsoftware.com>
2024-02-28 22:07:42 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+mod storage;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey")
+}
---
+++ src/storage/mod.rs
@@ -0,0 +1,2 @@
+pub mod cache_manager;
+pub mod index;
