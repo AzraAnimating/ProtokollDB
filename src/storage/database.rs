834f220131e1726bacfbe6734e046b8d074ea01a
Tobias <tobiasrempe@zyonicsoftware.com>
2024-02-29 10:40:42 +0100

    feat: Finished initial Setup
    - Added Protocol-database management
    - Added an annoying amount of foreign keys

--- src/storage/database.rs
+++ src/storage/database.rs
@@ -20,12 +20,21 @@ impl Database {
             CREATE TABLE IF NOT EXISTS 'examiners' (id INTEGER not null\nconstraint examiners_pk\nprimary key autoincrement, display_name TEXT not null);
             CREATE TABLE IF NOT EXISTS 'subjects' (id INTEGER not null\nconstraint subjects_pk\nprimary key autoincrement, display_name TEXT not null);
             CREATE TABLE IF NOT EXISTS 'stex' (id INTEGER not null\nconstraint stex_pk\nprimary key autoincrement, display_name TEXT not null);
-            CREATE TABLE IF NOT EXISTS 'subject_examiners' (
-                examiner_id INTEGER not null\nconstraint subject_examiners_examiners_id_fk\nreferences examiners,
-                subject_id INTEGER not null\nconstraint subject_examiners_subjects_id_fk\nreferences subjects,
-                stex_id INTERGER not null\nconstraint subject_examiners_stex_id_fk\nreferences stex
+            CREATE TABLE IF NOT EXISTS 'seasons' (id INTEGER not null\nconstraint seasons_pk\nprimary key autoincrement, display_name TEXT not null);
+            CREATE TABLE IF NOT EXISTS 'subject_relations' (
+                id INTEGER not null\nconstraint subject_relations_pk\nprimary key autoincrement,
+                examiner_id INTEGER not null\nconstraint subject_relations_examiners_id_fk\nreferences examiners,
+                subject_id INTEGER not null\nconstraint subject_relations_subjects_id_fk\nreferences subjects,
+                stex_id INTERGER not null\nconstraint subject_relations_stex_id_fk\nreferences stex,
+                season_id INTEGER not null\nconstraint subject_relations_seasons_id_fk\nreferences seasons,
+                year INTEGER not null
             );
-            ";//ToDo Create the other stuff :) 
+            CREATE TABLE IF NOT EXISTS 'protocols' (
+                id INTEGER not null\nconstraint protocols_pk\nprimary key autoincrement,
+                relation_id INTEGER not null\nconstraint protocols_subject_relations_id_fk\nreferences subject_relations,
+                protocol_uuid VARCHAR(36) not null
+            );
+            "; 
 
 
         match connection.execute(setup_query) {
