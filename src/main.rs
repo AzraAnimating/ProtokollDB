7bd7a1f7b1bcf1cfdfb43ea188a35ac215705b1f
Tobias <tobiasrempe@zyonicsoftware.com>
2024-02-29 16:57:04 +0100

    feat: Implemented Protocol Saving
    - Saving Protocols
    - Creating all the other stuff
    - Relationship Mapping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{sync::Arc, fs};
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use storage::database::Database;
@@ -8,6 +8,19 @@ mod storage;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let _ = fs::create_dir_all("protocols/");
+
+    println!("
+     __  __     ______     __     ______     ______     ______     ______    
+    /\\ \\_\\ \\   /\\  __ \\   /\\ \\   /\\  ___\\   /\\  __ \\   /\\  == \\   /\\  ___\\   
+    \\ \\  __ \\  \\ \\  __ \\  \\ \\ \\  \\ \\ \\____  \\ \\ \\/\\ \\  \\ \\  __<   \\ \\  __\\   
+     \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_____\\ 
+      \\/_/\\/_/   \\/_/\\/_/   \\/_/   \\/_____/   \\/_____/   \\/_/ /_/   \\/_____/ 
+                                                                             \n
+    H[AI]CORE : FSMED ProtocolDB (Backend) v0.0.1
+    Author: Tobias Rempe <tobias.rempe@rub.de>
+    Current Maintainer: Tobias Rempe <tobias.rempe@rub.de>");
 
     HttpServer::new(|| {
         App::new()
@@ -19,8 +32,14 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
+
 #[get("/")]
 async fn hello(data: web::Data<Arc<Mutex<Database>>>) -> impl Responder {
-    let res = data.lock().await.create_examiner("michael".to_string()).expect("Help");
-    HttpResponse::Ok().body(format!("{}", res.unwrap()))
+    let mut database = data.lock().await;
+    let examiner_id = database.create_examiner("Yuppimen".to_string()).expect("Help").unwrap();
+    let subject_id = database.create_subject("essi 2".to_string()).expect("Helpus").unwrap();
+    let stex_id = database.create_stex("17.".to_string()).expect("Helpas").unwrap();
+    let season_id = database.create_season("Tiefster Winter".to_string()).expect("Heldup").unwrap();
+    let protocol_id = database.save_protocol(examiner_id, subject_id, stex_id, season_id, 2024, "yeah HALLO".to_string()).expect("GLUGLUG").unwrap();
+    HttpResponse::Ok().body(format!("{}", protocol_id))
 }
--- src/storage/database.rs
+++ src/storage/database.rs
@@ -1,6 +1,8 @@
-use std::{io::Read, arch::x86_64::_andn_u32};
+use std::{io::Read, arch::x86_64::_andn_u32, fs, os::unix::fs::OpenOptionsExt};
 
+use actix_web::body::None;
 use sqlite::{Connection, Error, State};
+use uuid::Uuid;
 
 pub struct Database {
     connection: Connection
@@ -95,19 +97,103 @@ impl Database {
         };
 
         match potential_id {
-            Some(id) => {
-                return Result::Ok(Some(id));
-            },
-            None => {
-                return Result::Ok(None);
-            },
+            Some(id) => return Result::Ok(Some(id)),
+            None => return Result::Ok(None),
         }
 
     }
 
+
+    pub fn save_protocol(&mut self, examiner_id: i64, subject_id: i64, stex_id: i64, season_id: i64, year: i64, protocol: String) -> Result<Option<String>, Error> {
+        let protocol_uuid = match self.get_new_uuid() {
+            Some(uuid) => uuid,
+            None => return Result::Ok(None),
+        };
+        
+        match fs::write(format!("protocols/{}.txt", protocol_uuid), protocol) {
+            Ok(_) => {},
+            Err(err) => {
+                println!("Failed to write protocol to Disk!: {:?}", err); 
+                return Result::Ok(None);
+            },
+        };
+
+        let potential_relation_id = match self.create_relation(examiner_id, subject_id, stex_id, season_id, year) {
+            Ok(pot) => pot,
+            Err(err) => return Result::Err(err),
+        };
+
+        let relation_id = match potential_relation_id {
+            Some(id) => id,
+            None => return Result::Ok(None),
+        };
+        
+
+        match self.connection.execute(format!("INSERT INTO protocols(relation_id, protocol_uuid) VALUES ({}, '{}')", relation_id, protocol_uuid)) {
+            Ok(_) => {},
+            Err(err) => return Result::Err(err),
+        };
+
+
+        Result::Ok(Some(protocol_uuid.to_string()))
+    }
+
+    /// This method returns a UUID that is Unique (in this database)
+    /// Potentially it can recourse an infinite amount of times, but thats statistically VERY VERY
+    /// VERY UNLIKELY
+    fn get_new_uuid(&self) -> Option<String> {
+        let potential_uuid = Uuid::new_v4().to_string();
+
+        let potential_id = match self.if_exists(&format!("SELECT id FROM protocols WHERE protocol_uuid = '{}';", potential_uuid)) {
+            Ok(exists) => exists,
+            Err(err) => {
+                println!("Failed to check if uuid exists: {:?}", err); 
+                return None;
+            }
+        };
+
+        match potential_id {
+            Some(_) => return self.get_new_uuid(),
+            None => return Some(potential_uuid),
+        };
+    }
     
 
-    fn if_exists(&mut self, query: &str) -> Result<Option<i64>, Error> {
+    fn create_relation(&mut self, examiner_id: i64, subject_id: i64, stex_id: i64, season_id: i64, year: i64) -> Result<Option<i64>, Error> {
+        let query = format!("SELECT id FROM subject_relations WHERE examiner_id = {} AND subject_id = {} AND stex_id = {} AND season_id = {} AND year = {};", examiner_id, subject_id, stex_id, season_id, year);
+    
+        let potential_id = match self.if_exists(&query) {
+            Ok(exists) => exists,
+            Err(err) => {
+                return Result::Err(err)
+            }
+        };
+
+        match potential_id {
+            Some(id) => return Result::Ok(Some(id)),
+            None => {},
+        }
+
+        match self.connection.execute(format!("INSERT INTO subject_relations(examiner_id, subject_id, stex_id, season_id, year) VALUES ({}, {}, {}, {}, {});", examiner_id, subject_id, stex_id, season_id, year)) {
+            Ok(_) => {},
+            Err(err) => return Result::Err(err),
+        }
+
+        let potential_id = match self.if_exists(&query) {
+            Ok(exists) => exists,
+            Err(err) => {
+                return Result::Err(err)
+            }
+        };
+
+        match potential_id {
+            Some(id) => return Result::Ok(Some(id)),
+            None => return Result::Ok(None),
+        }
+
+    }
+
+    fn if_exists(&self, query: &str) -> Result<Option<i64>, Error> {
         let mut statement = match self.connection.prepare(query) {
             Ok(statement) => statement,
             Err(err) => {
